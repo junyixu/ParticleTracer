6e335c61680ea572056ae3c3265effe77668423c
Junyi Xu <junyixu0@gmail.com>
2023-12-18 02:14:59 +0800

    chore!: 调整目录结构
    使用方法： ./run.sh
    或在根目录 julia src/main.jl

---
+++ run.sh
@@ -0,0 +1,2 @@
+#! /bin/sh
+julia src/main.jl
--- src/main.jl
+++ src/main.jl
@@ -9,7 +9,9 @@ using HDF5
 using PyCall
 using LaTeXStrings
 # %%
+const output_dir = "output/"
 
+# %%
 @pyimport matplotlib.pyplot as plt
 
 # %%
@@ -25,7 +27,7 @@ function main(P::Parameters)
 			x[i],p[i]=P.pusher(x[i-1],p[i-1])
 		end
 		x[1],p[1] = P.pusher(x[end], p[end])
-		h5open("$(P.filename)$j.h5", "w") do f
+		h5open("$(output_dir*P.filename)$j.h5", "w") do f
 			f["x"]=x
 			f["p"]=p
 		end
@@ -37,12 +39,12 @@ main(p)
 
 # %%
 
-plot_phase("$(p.filename)1.h5", color="b")
-plot_phase("$(p.filename)$(p.nfiles).h5", color="r")
+plot_phase("$(output_dir*p.filename)1.h5", color="b")
+plot_phase("$(output_dir*p.filename)$(p.nfiles).h5", color="r")
 plt.axis("equal")
 plt.savefig("./figures/$(p.filename).pdf", bbox_inches="tight")
 plt.show()
-plot_E("$(p.filename)1.h5", color="b")
-plot_E("$(p.filename)$(p.nfiles).h5", color="r")
+plot_E("$(output_dir*p.filename)1.h5", color="b")
+plot_E("$(output_dir*p.filename)$(p.nfiles).h5", color="r")
 plt.savefig("./figures/$(p.filename)E.pdf", bbox_inches="tight")
 plt.show()
