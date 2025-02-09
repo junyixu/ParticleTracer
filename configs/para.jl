c0552b80ecf8a351c330ad8114716c2d016710ad
Junyi Xu <junyixu0@gmail.com>
2025-02-09 17:43:29 +0800

    fix!: 采用和 APT 相同的单位制 (KISS)
    也就是说 main 里面的单位制保持无单位制
    (逃逸电子在计算机内的 X 大约是 2000 量级)
    
    需要用户在 user_input.jl 自行换算无单位制

--- configs/para.jl
+++ configs/para.jl
@@ -1,18 +1,18 @@
 # simulation parameters
 Δt = 1.0e-1
-N = 8 # number of particles
+N = 50000 # number of particles
 # TotalSteps = 4000000 # total steps
 # SavePerNSteps = 10000 # Save 1000 steps
 TotalSteps = 100 # total steps
-SavePerNSteps = 1 # Save 1000 steps
+SavePerNSteps = 10 # Save 1000 steps
 
 # tokamak parameters
 B0 = 2.0 # Magnetic strength (T)
 u = Unit(B0)
-B0 = B0 * u.B
-E0 = 2.0 * u.E # Electric strength (V/m)
-R0 = 1.7 * u.x # Major radius of torus (m)
-a  = 0.4 * u.x # Minor radius of torus (m)
+B0 = B0 / u.B
+E0 = 2.0 / u.E # Electric strength (V/m)
+R0 = 1.7 / u.x # Major radius of torus (m)
+a  = 0.4 / u.x # Minor radius of torus (m)
 
 # initial conditions
 
